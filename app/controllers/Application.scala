b8b2cadc54eb1d6ffd86d258e00b559821e3a172
a-sir <alexander.sirenko@gmail.com>
2014-06-22 17:16:45 +0700

    Create sessid with first visit.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,18 +1,33 @@
 package controllers
 
+import akka.util.Timeout
 import messaging.BackendLookup
 import play.api.mvc._
 import results._
 
+import scala.util.Random
+
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index())
+    request =>
+      request.session.get("id").map { id =>
+        Ok(views.html.index(id))
+      }.getOrElse {
+        val sessionId = Random.nextString(10)
+        println("New user gets id:" + sessionId)
+        Ok(views.html.index(sessionId)).withSession("id" -> sessionId)
+      }
   }
 
   def request(query: String) = Action {
-      BackendLookup.lookupActor ! query
-      Ok(views.html.requested(query))
+    request =>
+      request.session.get("id").map { id =>
+        BackendLookup.lookupActor ! "query:" + query.trim
+        Ok(views.html.requested(query, id))
+      }.getOrElse {
+        Ok("Not authorized request. Go to mainpage.").withNewSession
+      }
   }
 
   def result() = Action {
